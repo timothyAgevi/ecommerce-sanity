b856aa912b6782f25d5e1df5562819a999e1621b
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 15:27:34 +0300

    update index page

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -3,7 +3,20 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    HeroBanner
+   
+    <div>
+      <h2> Best selling Products</h2>
+      <p> Speakers of many variants</p>
+    </div>
+    <div>
+      {
+        ['Product 1','Product 2'].map(
+          (product)=>product
+        )
+      }
+    </div>
+
+    footer
     </>
   )
 }
