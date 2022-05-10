317c42832ad8b72886d5a04b58f42fff471b9b33
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 22:36:56 +0300

    distracture footer banner elements

--- ecommerce/components/FooterBanner.jsx
+++ ecommerce/components/FooterBanner.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Link from'next/link';
 import {urlFor} from '../lib/client';
 
-const FooterBanner = ({footerBanner}) => {
+const FooterBanner = ({footerBanner:{ discount,largeText1,largeText2,saleTime,smallText,midtext,product,buttonText,image} }) => {
   return (
     <div className='footer-banner-container'> 
     <div className='banner-desc'> 
     <div className='left'>
-      
-      
+      <p> {footerBanner.discount}</p>
+      <h3> {footerBanner.largeText1}</h3>
+      <h3> {footerBanner.largeText2}</h3>
+      <p> {footerBanner.saleTime}</p>
        </div>
     <div className='right'></div>
     </div>
