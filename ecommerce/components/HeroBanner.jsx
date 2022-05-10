e82adce1ae35d688ef939c499ef99469576f2370
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 20:53:17 +0300

    add largetext to hero banner

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -6,6 +6,7 @@ const HeroBanner = ({heroBanner}) => {
 <div>
   <p className='beats-solo'> {heroBanner.smallText}</p>
   <h3>{heroBanner.midText}</h3>
+  <h1> {heroBanner.largeText1}</h1>
   <img src="" alt="headphones"
   className='hero-banner-image'/>
   <div>
