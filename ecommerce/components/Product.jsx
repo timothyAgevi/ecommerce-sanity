77303289739ca6f5218a056971adf797947ec56c
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 21:58:48 +0300

    add link properties to product component

--- ecommerce/components/Product.jsx
+++ ecommerce/components/Product.jsx
@@ -5,16 +5,18 @@ import { urlFor } from '../lib/client';
 const Product = ( {product: {image,name,slug,price} }) => {
   return (
     <div>
-      <link href={'/product/${slug.current}'}> 
+      <Link href={`/product/${slug.current}`}> 
       <div className='product-card'>
 <img
-src={ urlFor(image &&image[0])}
+src={ urlFor(image && image[0])}
 width={250}
 height={250}
 className='product-image'
 />
+<p className='product-name'>{name} </p>
+<p className='product-price'>${price}</p>
       </div>
-      </link>
+      </Link>
     </div>
   )
 }
