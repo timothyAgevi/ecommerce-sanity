65c8630828dcced50c310e6464b2528eda6f8528
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 16:40:28 +0300

    style for products-heading

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return ( <>
-       <div>
+       <div className='products-heading'>
       <h2> Best selling Products</h2>
       <p> Speakers of many variants</p>
     </div>
