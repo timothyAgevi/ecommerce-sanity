3298d5cd854c4072d5b9c37ba99e3b92f93f4a20
timothyAgevi <timothyagevi@gmail.com>
2022-05-10 20:39:20 +0300

    add heroBanner props

--- ecommerce/components/HeroBanner.jsx
+++ ecommerce/components/HeroBanner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link';
-const HeroBanner = () => {
+const HeroBanner = ({heroBanner}) => {
   return (
     <div className='hero-banner-container'>
 <div>
-  <p className='beats-solo'> SMALL TEXT</p>
+  <p className='beats-solo'> {heroBanner.smallText}</p>
   <h3> MID TEXT</h3>
   <img src="" alt="headphones"
   className='hero-banner-image'/>
