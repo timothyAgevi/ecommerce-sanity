7d274ce161230de52c479be56c418c614732d24b
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 12:23:43 +0300

    add rights reserved on footer

--- ecommerce/components/Footer.jsx
+++ ecommerce/components/Footer.jsx
@@ -3,6 +3,7 @@ import {AiFillInstagram,AiOutlineTwitter} from 'react-icons/ai'
 function Footer() {
   return (
     <div className='footer-container'>
+<p> 2022 shambaCoin All rights reserved</p>
 
     </div>
   )
