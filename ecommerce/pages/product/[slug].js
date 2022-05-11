8dbbcc806a3ff901eafc3dde20a72fa3885887cc
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 14:06:58 +0300

    add product details

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -8,15 +8,25 @@ const ProductDetails = ({product,products}) => {
 <div className='image-container'>
 <img src={urlFor(image && image[0])} />
 </div>
+<div className='small-images-container'>
+{image?.map( (item,i)=>(
+    <img
+    src={urlFor(item)}
+    className=""
+    onMouseEnter=""/>
+))}
+</div>
         </div>
     </div>
   )
 }
+
+
 //getStaticPaths function
 export const getStaticPaths=async ()=>{
     const query=`*[_type=="product"]{
 
-        slig{
+        slug{
             current
         }
     }`;
