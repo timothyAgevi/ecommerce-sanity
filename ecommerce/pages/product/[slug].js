5207f924f2a1b45b4bf89b06939c9b222a0b9937
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 14:08:35 +0300

    temporarly comment out small-images-container

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -8,14 +8,14 @@ const ProductDetails = ({product,products}) => {
 <div className='image-container'>
 <img src={urlFor(image && image[0])} />
 </div>
-<div className='small-images-container'>
+{/* <div className='small-images-container'>
 {image?.map( (item,i)=>(
     <img
     src={urlFor(item)}
     className=""
     onMouseEnter=""/>
 ))}
-</div>
+</div> */}
         </div>
     </div>
   )
