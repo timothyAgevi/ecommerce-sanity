b519f8759957b4c67700f777b2591a0a85107b9c
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 11:28:53 +0300

    update navbar component

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Link from 'next/link';
-import {AiOutlineShopping}from 'react-icons';
+import {AiOutlineShopping}from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div >Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/"> JSM Headphones</Link>
+      </p>
+      <button type='button' className='cart-icon' onClick="" >
+         <AiOutlineShopping/>
+         <span className='cart-item-qty'> 1</span>
+      </button>
+    </div>
   )
 }
 
