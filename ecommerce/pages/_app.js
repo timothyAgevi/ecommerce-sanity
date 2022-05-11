81c7feecc93a9c9f2c068e0933d76d8e01581f14
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 16:40:05 +0300

    add Toaster to enable pop up on cart

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Toaster}from 'react-hot-toast';
 import '../styles/globals.css'
 import {Layout} from '../components'
 import { StateContext}from '../context/StateContext';
