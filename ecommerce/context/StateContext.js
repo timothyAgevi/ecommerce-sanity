fe3c87a234b8a03be3a450c96deae9af5279a984
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 16:14:19 +0300

    add context hook  and functional component

---
+++ ecommerce/context/StateContext.js
@@ -0,0 +1,8 @@
+import react ,{createContext,useContext,useState,useEffect}from 'react';
+import {toast} from 'react-hot-toast';
+//createContext hook
+const Context=createContext();
+//context functional component
+export const StateContext=()=>{
+    
+}
