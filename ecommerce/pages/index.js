05bad63dda84225779a3af8d57fd02690ea62c93
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 11:59:07 +0300

    update product component

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,40 +1,35 @@
-import React from 'react'
-import{ Product,FooterBanner,HeroBanner}from '../components';
-import {client} from '../lib/client';
-const Home = ({products,bannerData}) => {
-  return ( 
-  <>
-  <HeroBanner heroBanner={bannerData.length&& bannerData[0]}
-  />
-  
-       <div className='products-heading'>
-      <h2> Best selling Products</h2>
-      <p> Speakers of many variants</p>
-    </div>
-    <div className='products-container'>
-      {
-        products?.map(
-          (product)=> <Product key={product._id} product={product}/>
-        )
-      }
+import React from 'react';
+
+import { client } from '../lib/client';
+import { Product, FooterBanner, HeroBanner } from '../components';
+
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]}  />
+    <div className="products-heading">
+      <h2>Best Seller Products</h2>
+      <p>speaker There are many variations passages</p>
     </div>
 
-    <FooterBanner footerBanner={bannerData && bannerData[0] }/>
-    </>
-  )
-  
-}
-// fetch function
-export const getServerSideProps=async()=>{
-  const query ='*[_type=="product"]';
-  const products =await client.fetch(query);
+    <div className="products-container">
+      {products?.map((product) => <Product key={product._id} product={product} />)}
+    </div>
 
-  const bannerQuery ='*[_type=="banner"]';
-  const bannerData =await client.fetch(bannerQuery);
+    <FooterBanner footerBanner={bannerData && bannerData[0]} />
+  </div>
+);
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
 
   return {
-    props:{products,bannerData}
+    props: { products, bannerData }
   }
 }
 
 export default Home;
+
