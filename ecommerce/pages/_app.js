0f7d62cf853bccb45de9660fd0671d61b28bc17b
timothyAgevi <timothyagevi@gmail.com>
2022-05-11 16:38:39 +0300

    wrap _app.js with statecontext

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -5,9 +5,11 @@ import { StateContext}from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
   return(
+    <StateContext>
     <Layout>
   <Component {...pageProps} />
   </Layout> 
+  </StateContext>
   )
 }
 
