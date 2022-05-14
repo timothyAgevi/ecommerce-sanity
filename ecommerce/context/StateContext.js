5088b7d5c7f7affb3059cf508d8395502c808606
timothyAgevi <timothyagevi@gmail.com>
2022-05-15 00:53:16 +0300

    update add to cart function

--- ecommerce/context/StateContext.js
+++ ecommerce/context/StateContext.js
@@ -15,10 +15,10 @@ export const StateContext = ({ children }) => {
 //function to add to cart
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
-      
-    if(checkProductInCart) {
-      setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+    if(checkProductInCart) {
+     
 
       const updatedCartItems = cartItems.map((cartProduct) => {
         if(cartProduct._id === product._id) return {
