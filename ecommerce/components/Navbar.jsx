544819b7bededb68067495a23d2f780e1204a98c
timothyAgevi <timothyagevi@gmail.com>
2022-05-15 00:49:54 +0300

    enable button close/open cart

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -11,11 +11,11 @@ const Navbar = () => {
       <p className='logo'>
         <Link href="/"> JSM Headphones</Link>
       </p>
-      <button type='button' className='cart-icon' onClick="" >
+      <button type='button' className='cart-icon' onClick= { ()=>setShowCart(true)}>
          <AiOutlineShopping/>
          <span className='cart-item-qty'> {totalQuantities}</span>
       </button>
-      <Cart/>
+      { showCart &&<Cart/>}
     </div>
   )
 }
