cb330f174033de349f8342b8ee1e75d3dc7f3e35
timothyAgevi <timothyagevi@gmail.com>
2022-05-16 13:52:41 +0300

    add submit type to params

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -17,7 +17,9 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-    const params = {line_items: [
+    const params = {
+        submit_type:'pay',
+        line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
           price: '{{PRICE_ID}}',
@@ -26,7 +28,8 @@ export default async function handler(req, res) {
       ],
       mode: 'payment',
       success_url: `${req.headers.origin}/?success=true`,
-      cancel_url: `${req.headers.origin}/?canceled=true`}
+      cancel_url: `${req.headers.origin}/?canceled=true`
+    }
 
 
       // Create Checkout Sessions from body params.
