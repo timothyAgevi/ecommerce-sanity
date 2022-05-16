62bb22f25b8624f07cc261fbe9b854b465e31d95
timothyAgevi <timothyagevi@gmail.com>
2022-05-16 14:02:03 +0300

    add first shipping rate

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -8,6 +8,10 @@ export default async function handler(req, res) {
         mode:'payment',
         payment_method_types:[ 'card'],
         billing_address_collection:'auto',
+        shipping_options:[
+            { shipping_rate: 'shr_1L01fmKR6HN9VIbuNiQ8rFY9'},
+            { shipping_rate: ''}
+        ],
         line_items: [
         {
           // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
