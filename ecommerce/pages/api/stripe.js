9efb58bb74ac78744535d5964c8c731138c33a1b
timothyAgevi <timothyagevi@gmail.com>
2022-05-16 13:56:16 +0300

    delete commented out code

--- ecommerce/pages/api/stripe.js
+++ ecommerce/pages/api/stripe.js
@@ -1,17 +1,3 @@
-// import Stripe from 'stripe';
-
-// const stripe =new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
-// //file handler
-// export default async function handler(req,res){
-// if(req.method==='POST'){
-//     try {
-        
-//     } catch (error) {
-//        res.status(500).json( {statusCode:500,message:error.message}) 
-//     }
-// }
-// }
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
